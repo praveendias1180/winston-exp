29ee1914c3b427d23788eada6e08bbbeb34625ed
Praveen Dias <praveendias1180@gmail.com>
2022-04-08 05:28:24 +0530

    exception handlers 🏁

--- clean.sh
+++ clean.sh
@@ -1,5 +1,6 @@
 rm combined.log
 rm error.log
 rm all.log
+rm exception.log
 
 echo "Cleared the logs."
---
+++ example-1.js
@@ -0,0 +1,48 @@
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = createLogger({
+  level: 'silly',
+  format: combine(
+    label({ label: 'rangequest'}),
+    timestamp(),
+    myFormat
+  ),
+  defaultMeta: { service: 'user-service', username: 'Praveen' },
+  transports: [
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log', level: 'info' }),
+    new transports.File({ filename: 'all.log' }),
+  ],
+  exceptionHandlers: [
+    new transports.File({ filename: 'exception.log' })
+  ]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new transports.Console({
+    format: format.combine(
+        format.colorize(),
+        format.simple()
+    ),
+  }));
+}
+
+logger.log('silly', "127.0.0.1 - welcome to rangequest");
+logger.log('debug', "127.0.0.1 - welcome to rangequest");
+logger.log('verbose', "127.0.0.1 - welcome to rangequest");
+logger.log('info', "127.0.0.1 - welcome to rangequest");
+logger.log('warn', "127.0.0.1 - welcome to rangequest");
+logger.log('error', "127.0.0.1 - welcome to rangequest");
+logger.info("127.0.0.1 - welcome to rangequest");
+logger.warn("127.0.0.1 - welcome to rangequest");
+logger.error("127.0.0.1 - welcome to rangequest");
+
+// throw 'Exception occured';
+throw new Error('Error occured');
+console.log('Execution Finished');
+
